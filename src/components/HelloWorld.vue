ab0654da2670e70cda1fcd92cc9befbdcb715032
DayLoveNight <1529833211@qq.com>
2020-09-09 18:02:39 +0800

    second-commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container box1">
+    <div class="row">
+      <div class="col-md-2 col-sm-12 child">1</div>
+      <div class="col-md-2 col-sm-12 child">2</div>
+
+      <div class="col-md-2 col-sm-12 child">3</div>
+      <div class="col-md-2 col-sm-12 child">4</div>
+      <div class="col-md-2 col-sm-12 child">5</div>
+      <div class="col-md-2 col-sm-12 child">6</div>
+
+      <div class="col-sm-12 child">1</div>
+      <div class="col-sm-12 child">2</div>
+
+      <div class="col-sm-12 child">3</div>
+      <div class="col-sm-12 child">4</div>
+      <div class="col-sm-12 child">5</div>
+      <div class="col-sm-12 child">6</div>
+      erci
+    </div>
+  </div>
+</template>
+<style>
+.box1 {
+  background: #ff6700;
+  color: white;
+  min-height: 200px;
+}
+</style>
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
